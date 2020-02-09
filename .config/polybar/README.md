a5e9547a6641871dc5d111e0a95fcd0ee1ac00f4
serafss2 <fares@bessrour.com>
2020-02-09 04:29:11 -0500

    Added my WM and it's UI stuff

---
+++ .config/polybar/README.md
@@ -0,0 +1,4 @@
+# Polybar(-6) THEME copy-pasted from https://github.com/adi1090x/polybar-themes
+
+You may reuse this under GPL-3.0, same as the original.
+
--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,7 +8,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar -r bottom &
-
-echo "Polybar launched..."
+# Launch bar1 and bar2
+polybar -c ~/.config/polybar/config.ini main &
