7b0a298b934bcf452a2f3c5559088ebdc0c35f83
فارس <fares@bessrour.com>
2020-11-02 14:49:22 -0500

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,14 @@
 # Set PATH variable
 if status --is-login
     set -x PATH $PATH ~/bin
+    
+    # Kubernetes
+    alias k=kubectl
+    alias kpods="kubectl get pods"
+    alias kcj="kubectl get cj"
+    
+    # Linux tools
+    alias watch="watch -n 0.2"
 end
 
 function on_exit --on-event fish_exit
