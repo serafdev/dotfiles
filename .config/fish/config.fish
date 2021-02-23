c25d2d866bd4a4f31de6212154926ce771032d75
فارس <fares@bessrour.com>
2021-02-23 16:25:11 -0500

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,10 +5,24 @@ end
 # PATH
 set -gx PATH $PATH ~/bin
 set -gx PATH $PATH $HOME/.krew/bin
-
+set -gx PATH $PATH $HOME/.cargo/bin
 # k8s
-alias k=kubectl
-
+function k
+    command kubectl $argv
+end
 
 # Linux tools
-alias watch="watch -n 0.2 "
+# alias watch="watch -n 0.2 -x fish -c "
+
+function watch
+    command watch -n 0.2 -x fish -c "$argv"
+end
+
+alias w=watch
+
+alias kev="oc get events --sort-by='.metadata.creationTimestamp'"
+alias kpr="kubectl get pods --field-selector=status.phase=Running"
+
+starship init fish | source
+
+alias python=python3
